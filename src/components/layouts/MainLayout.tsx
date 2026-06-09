a01649a640ddf9a7a3f9b11edd6d26d6191aa7b8
Sajidur Rahman <sajidur.inbox@gmail.com>
2026-06-10 03:14:25 +0600

    added: motion in route

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,12 +1,25 @@
 import { Footer, MainBanner, Nav } from '@/components/common/index'
-import { Outlet } from 'react-router-dom'
+import { motion, useReducedMotion } from 'framer-motion'
+import { Outlet, useLocation } from 'react-router-dom'
 
 const MainLayout = () => {
+    const location = useLocation()
+    const prefersReducedMotion = useReducedMotion()
+
     return (
         <div className="max-w-[55ch] mx-auto px-4 md:px-0">
             <Nav />
             <MainBanner />
-            <Outlet />
+            <motion.div
+                key={location.pathname}
+                initial={
+                    prefersReducedMotion ? false : { opacity: 0, y: 6 }
+                }
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.2, ease: 'easeOut' }}
+            >
+                <Outlet />
+            </motion.div>
             <Footer />
         </div>
     )
